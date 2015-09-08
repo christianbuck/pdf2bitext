57c100954533ff8cc8e264b87485ba25db897bfd
Christian Buck <cbuck@lantis.de>
2015-09-08 16:16:01 +0200

    moved section

--- languagestripper.py
+++ languagestripper.py
@@ -23,9 +23,9 @@ class LanguageStripper(object):
             self.code_to_language[code] = "fr"
         for code in ["chinese", "chi", "zh"]:
             self.code_to_language[code] = "zh"
+        # new, not in "Dirt-Cheap"-paper
         for code in ["russian", "russky", "russki", "russkij", "rus", "ru"]:
             self.code_to_language[code] = "ru"
-        # new, not in "Dirt-Cheap"-paper
         for code in ["tedesco", "de-de", "de-ch", "de-at", "de-li", 'de-lu',
                      'allemand']:
             self.code_to_language[code] = "de"
